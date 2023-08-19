a71cad3b151f919e50396e1e977051087cf7188d
Sri Vishnu <srivishp@gmail.com>
2023-08-20 00:05:33 +0530

    Added state management & created context

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom";
 import classes from "./Modal.module.css";
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop}></div>;
+  return <div className={classes.backdrop} onClick={props.onClose}></div>;
 };
 
 const ModalOverlay = (props) => {
@@ -18,7 +18,10 @@ const portalElementHelper = document.getElementById("overlays");
 const Modal = (props) => {
   return (
     <>
-      {ReactDOM.createPortal(<Backdrop />, portalElementHelper)}
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElementHelper
+      )}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElementHelper
