bc839fa9de5524e4ec511cf20a474d109da8daf8
Sri Vishnu <srivishp@gmail.com>
2023-08-20 23:39:41 +0530

    Adding items to Cart array

--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -1,13 +1,14 @@
+import React from "react";
 import classes from "./Input.module.css";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
       {/* Using spread operator to add all the values of props at once */}
-      <input {...props.input} />
+      <input ref={ref} {...props.input} />
     </div>
   );
-};
+});
 
 export default Input;
