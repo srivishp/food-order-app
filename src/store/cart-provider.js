ea8656985d2b231efb82bc169a40f69e37063746
Sri Vishnu <srivishp@gmail.com>
2023-08-21 08:38:03 +0530

    Functionality for + & - buttons in Cart

--- src/store/cart-provider.js
+++ src/store/cart-provider.js
@@ -14,7 +14,7 @@ const cartReducer = (state, action) => {
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
     );
-    const existingCartItem = state.items.concat[existingCartItemIndex];
+    const existingCartItem = state.items[existingCartItemIndex];
 
     let updatedItems;
 
@@ -31,6 +31,30 @@ const cartReducer = (state, action) => {
     }
 
     return { items: updatedItems, totalAmount: updatedTotalAmount };
+  }
+  if (action.type === "REMOVE_FROM_CART") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+    let updatedItems;
+    if (existingCartItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount - 1,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
   }
 
   return defaultCartState;
